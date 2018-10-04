04bff715849f01718475f7711322997ce0698a74
Luke Brinkerhoff <35081523+blaknite5@users.noreply.github.com>
2018-10-03 21:20:58 -0700

    Added Hidden Cheat menu
    Type "pop" to open menu

---
+++ src/Chasing the End/Assets/Luke/hiddenMenu.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class hiddenMenu : MonoBehaviour
+{
+
+    private string[] cheatCode;
+    private int index;
+    private bool cheat;
+    private Camera m_MainCamera;
+
+    void OnGUI()
+    {
+        if (cheat)
+        {
+            m_MainCamera = Camera.main;
+            // draw the hidden menu
+            GUI.Box(new Rect(20, 80, 240, 240), "Cheat Menu :-) ");
+            if (GUI.Button(new Rect(40, 120, 200, 20), "Return to Main Menu"))
+            {
+                SceneManager.LoadScene(0);
+            }
+            if (GUI.Button(new Rect(40, 150, 200, 20), "Reset Level"))
+            {
+                SceneManager.LoadScene(1);
+            }
+            if (GUI.Button(new Rect(40, 180, 200, 20), "Toggle Guide Path"))
+            {
+                m_MainCamera.cullingMask ^= 1 << LayerMask.NameToLayer("hiddenPath");
+            }
+            if (GUI.Button(new Rect(40, 180, 200, 20), "Turn on Path"))
+            {
+// m_MainCamera.cullingMask &= 1 ~(<< LayerMask.NameToLayer("hiddenPath"));
+            }
+
+            if (GUI.Button(new Rect(40, 270, 200, 20), "Close this menu"))
+            {
+                cheat = false;
+                index = 0;
+            }
+        }
+    }
+
+    void Start()
+    {
+        // The pass Code is "secret", user needs to input 
+        // this word to show the hidden menu
+        cheatCode = new string[] {
+   "p",
+   "o",
+   "p",
+  };
+        index = 0;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        if (Input.anyKeyDown)
+        {
+            if (Input.GetKeyDown(cheatCode[index]))
+            {
+                // right input, check next digit
+                index++;
+            }
+            else
+            {
+                // wrong input, restart from index 0
+                index = 0;
+            }
+        }
+
+        if (index == cheatCode.Length)
+        {
+            // cheat is ok, the hidden menu is now visible :-)
+            cheat = true;
+            index = 0;
+        }
+    }
+}

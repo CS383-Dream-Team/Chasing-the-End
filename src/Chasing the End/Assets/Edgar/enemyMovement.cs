094f8cb55b74067456dedbdf31ccb8804a9622fa
Edgar <edgarvelazz23@gmail.com>
2018-10-08 16:03:36 -0700

    Enemy now turns left and right

--- src/Chasing the End/Assets/Edgar/enemyMovement.cs
+++ src/Chasing the End/Assets/Edgar/enemyMovement.cs
@@ -7,6 +7,8 @@ public class enemyMovement : MonoBehaviour {
     public float speed;         //Moving speed for the enemy
     private Transform player;   //The player
     public float distanceToStop;
+    private bool facingRight = false;
+
 
 	void Start () {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();      //Object with the player tag
@@ -14,9 +16,27 @@ public class enemyMovement : MonoBehaviour {
 	
 	
 	void Update () { //Move the enemy towsrds the player
+        float horizontal = Input.GetAxis("Horizontal");
         if (Vector2.Distance(transform.position, player.position) > distanceToStop) //If statement added so that the enemy is not on top of the player when they collide
         {                                                                   //If the distance is > than 1.5 enemy will continue moving
             transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
         }
+      if(player.position.x > transform.position.x && !facingRight){
+     Debug.Log("in the loop");
+     Flip();
+     facingRight=true;
+     }else if(player.position.x < transform.position.x && facingRight ){
+      Debug.Log("in the different loop");
+      Flip();
+      facingRight=false;
+     //transform.localScale = new Vector3(-1,1,1);
+     }
+        
 	}
+    private void Flip()
+        {
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
 }

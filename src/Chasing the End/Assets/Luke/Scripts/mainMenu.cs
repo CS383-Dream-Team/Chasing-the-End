74c613c8b8cdb2d4d5a45fd40513c4828a082874
Luke Brinkerhoff <35081523+blaknite5@users.noreply.github.com>
2018-10-25 08:15:52 -0700

    Added Menu and inital test scenes
    Modified build settings for a test build and default build.

---
+++ src/Chasing the End/Assets/Luke/Scripts/mainMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour {
+
+    public void loadTest1()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void loadTest2()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void loadTest3()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void loadTest4()
+    {
+        SceneManager.LoadScene(4);
+    }
+    public void loadTest5()
+    {
+        SceneManager.LoadScene(5);
+    }
+
+    public void quitTest()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+
+}

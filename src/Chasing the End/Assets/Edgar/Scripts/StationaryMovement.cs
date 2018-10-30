52ff41318cb3b250e321cdd9b9b47f40e3c6b007
Edgar <edgarvelazz23@gmail.com>
2018-10-29 17:21:40 -0700

    Added an enemy that moves only horizontally from wall to wall

---
+++ src/Chasing the End/Assets/Edgar/Scripts/StationaryMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StationaryMovement : MonoBehaviour {
+    private Transform Door;
+    private Vector3 target;
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name == "Collisions")
+        {
+            Flip();
+        }
+        if (col.gameObject.name == "Player")
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+    // Use this for initialization
+    void Start () 
+    {
+        target = new Vector3(-10.0F, transform.position.y, 0.0f);
+        {
+            Door = GameObject.FindGameObjectWithTag("Door").GetComponent<Transform>();
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update () 
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        transform.position = Vector2.MoveTowards (transform.position,target, 2 * Time.deltaTime);
+    }
+
+    private void Flip()
+    {
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+        target.x *= -1;
+    }
+}

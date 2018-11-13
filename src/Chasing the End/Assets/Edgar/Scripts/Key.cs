1028750f76c9db2894205e62be2f7744adcffad5
Edgar Velazquez <vela0695@vandals.uidaho.edu>
2018-11-12 19:07:10 -0800

    Keys can now be picked up

---
+++ src/Chasing the End/Assets/Edgar/Scripts/Key.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// Class used for the item script containg information for the inventory 
+/// contains the get name, get image and onpickup for the items.
+/// Must be added to each object that is to be added in the inventory in
+/// the same format but each with its name and image
+/// </summary>
+public class Key : MonoBehaviour, I_IventoryItem
+{
+
+
+    /// <summary>
+    /// each item must have a name that identifies it. 
+    /// </summary>
+    public string Name
+    {
+        get
+        {
+            return "Key";
+        }
+    }
+
+    /// <summary>
+    /// This is to set the image to the object of this script belongs to. 
+    /// Its passed from the inspector as a sprite image
+    /// </summary>
+    public Sprite imageFromObject = null;
+
+
+
+    /// <summary>
+    /// The Event is called upon a event conditon that is collision with this 
+    /// item. Upon it the object returns an image of the object as a Sprite. 
+    /// </summary>
+    /// <return> Sprite Image</return>
+    public Sprite Image
+    {
+
+        get
+        {
+            return imageFromObject;
+        }
+    }
+
+
+    /// <summary>
+    /// Item Event is called after the item has been added to do the inventory. 
+    /// </summary>
+    public void OnPickup()
+    {
+        gameObject.SetActive(false);
+    }
+}

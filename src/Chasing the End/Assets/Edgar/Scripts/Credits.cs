2c1ed82948cc6312dd6349a2d0610abb4db232b1
Edgar Velazquez <vela0695@vandals.uidaho.edu>
2018-11-27 15:54:04 -0800

    Updated credits scene

---
+++ src/Chasing the End/Assets/Edgar/Scripts/Credits.cs
@@ -0,0 +1,16 @@
+﻿/*
+ * Simple script to exit the credits scene and return
+ * to the main menu. Scene name is passed through the
+ * inspector on Unity.
+ */
+using UnityEngine;
+
+public class Credits : MonoBehaviour 
+{
+    // Name of scene is passed on the inspector in unity.
+    public void ChangeScene(string nameOfScene)
+    {
+        Application.LoadLevel(nameOfScene);
+    }
+
+}

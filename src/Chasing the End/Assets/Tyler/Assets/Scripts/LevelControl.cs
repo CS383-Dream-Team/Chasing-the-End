6b1ff959becbab50adea96d1912b58bfd3ff6c3b
Shinrabansh0 <felpskid2@yahoo.com>
2018-11-27 13:14:12 -0800

    Level/Credit Updates
    Added a few more enemies to earlier levels. Added music to credits. Added some comments to code for summary, waiting on some info to finish level progress.

--- src/Chasing the End/Assets/Tyler/Assets/Scripts/LevelControl.cs
+++ src/Chasing the End/Assets/Tyler/Assets/Scripts/LevelControl.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
+/// <summary>
+/// Allows for different scenes to be loaded after a trigger.
+/// In this case when the character with tag Player has specific items in their inventory
+/// the specified scene will load in. -Tyler
+/// </summary>
 public class LevelControl : MonoBehaviour {
 
     public int index;
+    //Must use scene name exactly to load correct scene
     public string levelName;
 
+    //Trigger for loading scenes
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if ((other.CompareTag("Player")))
         {
             SceneManager.LoadScene(index);
             SceneManager.LoadScene(levelName);

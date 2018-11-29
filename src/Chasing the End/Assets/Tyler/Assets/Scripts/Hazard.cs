da766f45dc9ea64b64676635dd7e1c11c8a3419e
Shinrabansh0 <felpskid2@yahoo.com>
2018-11-28 17:23:54 -0800

    Fixed Hazard Error
    Added hazard back to projectiles and fixed compiling error.

--- src/Chasing the End/Assets/Tyler/Assets/Scripts/Hazard.cs
+++ src/Chasing the End/Assets/Tyler/Assets/Scripts/Hazard.cs
@@ -2,6 +2,12 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/// <summary>
+/// Hazard Class is made to control projectile collision with the player.
+/// If the saw blade sprite collides with the player with tag "Player" then the Gameover scene is loaded. 
+/// If the saw blade contacts another game object, such as the enemies or a wall, the object is destroyed. 
+/// The saw blade is also destroyed if the set time is met.
+/// </summary>
 public class Hazard : MonoBehaviour
 {   
     void OnCollisionEnter2D(Collision2D coll)
@@ -10,7 +16,6 @@ public class Hazard : MonoBehaviour
         {
             SceneLoader sawBlade = new SceneLoader();
             sawBlade.Gameover();
-
         }
         else
         {
